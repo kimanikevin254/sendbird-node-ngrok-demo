bfb71e5e394213ee70c025c4dc1f31270ab02cd1
Kevin Kim <58376469+kimanikevin254@users.noreply.github.com>
2024-04-22 20:05:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 // Import packages
-const express = require('express')
-const fetch = require('node-fetch');
+const express = require("express");
+const fetch = require("node-fetch");
 
 // Create an instance of the server
-const app = express()
+const app = express();
 
 // Set view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-// Fetch a list of ten products and dispplay them on the index page
-app.get('/', async (req, res) => {
+// Fetch a list of ten products and display them on the index page
+app.get("/", async (req, res) => {
     try {
-        const response = await fetch('https://dummyjson.com/products?limit=10');
+        const response = await fetch("https://dummyjson.com/products?limit=10");
         const { products } = await response.json();
-        res.render('index', { products });
+        res.render("index", { products });
     } catch (error) {
-        console.error('Error fetching products:', error);
-        res.status(500).send('Internal Server Error');
+        console.error("Error fetching products:", error);
+        res.status(500).send("Internal Server Error");
     }
-})
+});
 
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000')
-})
+    console.log("Server is listening on port 5000");
+});
+
